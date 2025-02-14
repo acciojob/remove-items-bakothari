0a9e55dc6c2f9dfab78ba7224dcbf1b0e8db9ca4
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-14 12:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 //your JS code here. If required.
-document.getElementsById("colorSelect").addEventListener("click", ()=> {
+document.getElementById("colorSelect").addEventListener("click", ()=> {
     const colorSelect = document.getElementById("colorSelect");
     const selectedIndex = colorSelect.selectedIndex;
-
-    
-    if (selectedIndex !== -1) {
+	   if (selectedIndex !== -1) {
         
         colorSelect.removeChild(colorSelect.options[selectedIndex]);
     }
