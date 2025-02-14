39e1340d26ed504215051681f287f09d50c954d1
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-14 12:58:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+const colorSelect = document.getElementById("colorSelect");
+ colorSelect.addEventListener("click", function () {
+const selectedIndex = colorSelect.selectedIndex;
 
-    const colorSelect = document.getElementById("colorSelect");
-     colorSelect.addEventListener("click", function () {
-    const selectedIndex = colorSelect.selectedIndex;
-
-    if (selectedIndex !== -1) {
-        colorSelect.remove(colorSelect.options[selectedIndex]);
+ if (selectedIndex !== -1) {
+    colorSelect.remove(colorSelect.options[selectedIndex]);
     }
 });
