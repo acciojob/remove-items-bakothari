f7aa0a22e882a8d681e1179d7aba13385ffe5f0c
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-14 12:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementsById("colorSelect").addEventListener("click", function () {
+    const colorSelect = document.getElementById("colorSelect");
+    const selectedIndex = colorSelect.selectedIndex;
+
+    
+    if (selectedIndex !== -1) {
+        
+        colorSelect.removeChild(colorSelect.options[selectedIndex]);
+    }
+});
