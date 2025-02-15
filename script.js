18b50d1360f7b10283aa57c308a74f11792de237
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-15 15:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const colorSelect = document.getElementById("colorSelect");
-let btn = document.querySelector("input")
+let btn = document.querySelector("input[type='button']");
+
 btn.addEventListener("click", () => {
     const selectedIndex = colorSelect.selectedIndex;
-
-    if (selectedIndex !== -1) {
-        colorSelect.remove(colorSelect.options[selectedIndex]);
-    }
+     colorSelect.remove(selectedIndex);
+    
 });
